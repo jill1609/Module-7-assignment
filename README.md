a2eefe16f27e440574ee8f0318ab8caf4ee538f7
unknown <jill.popat@yahoo.com>
2023-01-17 21:54:57 -0500

    assignment 7 complete

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# Module-7-assignment
+# Module-7-assignment (Markdown report)
+
+## Part 1
+
+**There are a total of 350 transactions which are below $2.00**. Hence, this is clear evidence that credit cards have been hacked, as this number is astonishingly high. 
+
+However, it should be noted that when looking at transactions **between 7 am to 9 am for the year 2018**, there do not appear to be any fraudulent transactions in the top 100 transactions. 
+
+But there is clear evidence that suggests that fraudulent transactions have been recorded during rest of the day.  
+
+>The top 5 merchants that are prone to fraudulent trnasactions are: **Wood-Ramirez; Hood-Phillips; Baker Inc; Jarvis-Turner; and Martinez Group.**  
+
+The views for all the queries have been prepared in the SQL file.
+
+*Lastly, the schema file and query file for SQL both contain the queries used for data analysis. This is because queries were originally prepared in the schema file, but for submission purposes a query file has been created as well.* 
+
+
+-----
+
+
+
+## Part 2
+
+### Data for card holder 2
+![cardholder2](cardholder2.png)
+
+>*As per the analysis, there definitely seems to be evidence of hacking for cardholder 2, as there are multiple transactions below $2.00 for cardholder 2*
+
+
+
+
+### Data for cardholder 18
+--------
+![cardholder18](cardholder18.png)
+
+>*In case of cardholder 18, there does not appear to be too many fraudulent transations, and the spending pattern is vastly different for this cardholder as well.* 
+
+---------
+
+### Comparison between cardholders 2 & 18
+![cardholder2&18](cardholders2&18.png)
+
+>*Here, in this line plot the spending patterns for cardholders 2 and 18 are compared, and it is clear that their spending patterns are significantly different. And this difference clearly shows that cardholder 2 has had their credit card hacked way more than cardholder 18.*  
+
+-------
+
+### Data for Cardholder 25
+![cardholder25](cardholder25bymonth.png)
+
+>*In case of cardholder 25, the data has been analysed on a monthly basis from January to June 2018 on a box plot. It should be noted that this cardholder's average spending pattern is transactions below $50 for each month. However, there are outliers within these transactions, which are quite high in comparison to the average spending pattern for this cardholder. These outliers have been recorded in each month from January to June 2018, except February. And the frequency of these outliers is in the range of 1-3 outlying transactions per month. Moreover, because of how vast these outlying transactions are, it is recommended that this box plot be referred to again in jupyter notebook (Python), in order to carry out futher inspection of monthly average spending patterns for cardholder 25.*    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ query for assignment 7.sql
@@ -0,0 +1,97 @@
+--Data Ananlysis through query
+--How can you isolate (or group) the transactions of each cardholder?
+SELECT transaction.date, transaction.amount, credit_card.card, card_holder.name
+FROM transaction
+INNER JOIN credit_card ON transaction.card=credit_card.card
+INNER JOIN card_holder ON credit_card.cardholder_id=card_holder.id
+ORDER BY card_holder.name;
+
+--Count the transactions that are less than $2.00 per cardholder.
+SELECT COUNT(amount)
+FROM transaction 
+WHERE amount<2.00
+
+--Is there any evidence to suggest that a credit card has been hacked? Explain your rationale.
+--It does appear that credit cards have been have hacked, since the there are 350 transactions that are less than $2.00.
+
+--What are the top 100 highest transactions made between 7:00 am and 9:00 am?   
+SELECT *
+FROM transaction AS t
+WHERE date_part('hour', t.date) >= 7 AND date_part('hour', t.date) <= 8
+ORDER BY amount DESC
+LIMIT 100;
+
+--Do you see any anomalous transactions that could be fraudulent?
+--No fraudulent transaction has been recorded between this timeframe of 7:00 am to 9:00 am.
+
+--Is there a higher number of fraudulent transactions made during this time frame versus the rest of the day?
+SELECT *
+FROM transaction
+WHERE amount<2.00
+ORDER BY amount ASC
+ --No, the timeframe of 7:00 am to 9:00 am has fewer fraudulent transactions than rest of the day.
+ 
+--What are the top 5 merchants prone to being hacked using small transactions?
+SELECT COUNT(t.amount), t.id_merchant, m.name, m.id 
+FROM transaction AS t
+INNER JOIN merchant AS m ON t.id_merchant=m.id
+WHERE amount<2.00
+GROUP BY t.id_merchant, m.name, m.id 
+ORDER BY COUNT(t.amount) DESC
+LIMIT 5
+
+--Create a view for each of your queries.
+CREATE VIEW cardholder_transactions AS
+SELECT transaction.date, transaction.amount, credit_card.card, card_holder.name
+FROM transaction
+INNER JOIN credit_card ON transaction.card=credit_card.card
+INNER JOIN card_holder ON credit_card.cardholder_id=card_holder.id
+ORDER BY card_holder.name;
+
+CREATE VIEW fraudulent_transactions AS
+SELECT COUNT(amount)
+FROM transaction 
+WHERE amount<2.00
+
+CREATE VIEW morning_transactions AS
+SELECT *
+FROM transaction AS t
+WHERE date_part('hour', t.date) >= 7 AND date_part('hour', t.date) <= 8
+ORDER BY amount DESC
+LIMIT 100;
+
+CREATE VIEW allday_fraudulent_transactions AS
+SELECT *
+FROM transaction
+WHERE amount<2.00
+ORDER BY amount ASC
+
+CREATE VIEW top_5_merchants_hacked AS
+SELECT COUNT(t.amount), t.id_merchant, m.name, m.id 
+FROM transaction AS t
+INNER JOIN merchant AS m ON t.id_merchant=m.id
+WHERE amount<2.00
+GROUP BY t.id_merchant, m.name, m.id 
+ORDER BY COUNT(t.amount) DESC
+LIMIT 5
+--
+
+--query for cardholder 2
+SELECT * FROM transaction t
+INNER JOIN credit_card c
+ON t.card = c.card
+WHERE cardholder_id = 2;
+
+--query for cardholder 18
+SELECT * FROM transaction t
+INNER JOIN credit_card c
+ON t.card = c.card
+WHERE cardholder_id = 18;
+
+--query for cardholder 25
+SELECT *
+FROM transaction t
+INNER JOIN credit_card c 
+ON t.card = c.card
+WHERE date BETWEEN '2018-01-01 00:00:00' AND '2018-06-30 23:59:59'
+AND cardholder_id = 25;
